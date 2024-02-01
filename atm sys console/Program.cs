78fdccbf9d54d62140a590646a856c2d28bcbe1b
corunesbhatti <bhicorni@gmail.com>
2024-02-01 07:41:17 +0500

    Initial commit

---
+++ atm sys console/Program.cs
@@ -0,0 +1,199 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Threading.Channels;
+public class cardHoulder
+{
+    string cardnum;
+    int pin;
+    string firstname;
+    string lastname;
+    double balance;
+
+    public cardHoulder(string cardnum, int pin, string firstname, string lastname, double balance) 
+    { 
+    this.cardnum = cardnum;
+    this.pin = pin;
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.balance = balance;
+    }
+    public string getnum(){
+        return cardnum;
+    }
+    public int getpin()
+    {
+        return pin;
+    }
+    public string getfirstname()
+    {
+        return firstname;
+    }
+    public string getlastname()
+    {
+        return lastname;
+    }
+    public double getbalance()
+    {
+        return balance;
+    }
+    
+    public void setnum(string newcardnum)
+    {
+        cardnum = newcardnum;
+    }
+    public void setpin(int newpin)
+    {
+        pin = newpin;
+    }
+    public void  setfirstname (string newfirstname)
+    {
+        firstname = newfirstname;
+    }
+    public void setladstname(string newlastname)
+    {
+        lastname = newlastname;
+    }
+    public void setbalance(double newbalance)
+    {
+        balance = newbalance;
+    }
+
+    public void main(string[] args)
+    {
+        void printOptions()
+        {
+            Console.WriteLine("welcome to ATM");
+            Console.WriteLine("please select one of those option !!!");
+            Console.WriteLine("1. Deposit");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. show balance");
+            Console.WriteLine("4. Exit");
+        }
+        void deposit(cardHoulder currentUser)
+        {
+            Console.WriteLine("How mush $$ would you like to deposit !!");
+            double dopsit = Double.Parse(Console.ReadLine());
+            currentUser.setbalance(currentUser.getbalance() + dopsit);
+            Console.WriteLine("Thankyou for depositing $$, Your new balance is:" + currentUser.getbalance());
+        }
+        void withdraw (cardHoulder currentUser)
+        {
+            Console.WriteLine("How mush $$ would you like to Withdraw !!");
+            double withdrawal = Double.Parse(Console.ReadLine());
+            //checked if user have enough balance to withdraw
+            if (currentUser.getbalance() > withdrawal)
+            {
+                Console.WriteLine("Sorry you can't withdraw, you don't have enough balance :( ");
+            }
+            else
+            {
+                currentUser.setbalance(currentUser.getbalance() - withdrawal);
+                Console.WriteLine("You're good to go! Thank you :)");
+            }
+            void balance (cardHoulder cuurentUser)
+            {
+                Console.WriteLine("your Current Balance is :" + currentUser.getbalance());
+            }
+
+            List<cardHoulder> cardhoulders = new List<cardHoulder>();
+            cardhoulders.Add(new cardHoulder("4238679324067835", 1343, "Sameed", "Behari", 17803.52));
+            cardhoulders.Add(new cardHoulder("5483679329757835", 1243, "Shabbir", "Taliban", 17833.52));
+            cardhoulders.Add(new cardHoulder("6783679367357835", 7343, "Usman", "binladir", 17853.52));
+            cardhoulders.Add(new cardHoulder("7983679390257835", 2343, "Asad", "dhobe", 178.52));
+            cardhoulders.Add(new cardHoulder("8903679389377835", 4343, "Kabber", "Pashah", 17383.52));
+            cardhoulders.Add(new cardHoulder("9026679357857835", 5343, "Ayan", "bhai", 17853.52));
+
+
+            //prompt user
+            Console.WriteLine("Welcome Sir/Madam/behari ");
+            Console.WriteLine("I hope you use this ATM in good health :)");
+            Console.WriteLine("please enter your detit card: ");
+            string detbitCardNum = "";
+            cardHoulder currentUsers;
+
+            while (true)
+            {
+                try
+                {
+                    detbitCardNum = Console.ReadLine();
+                    //check in users list
+                    currentUser = cardhoulders.FirstOrDefault(a => a.cardnum == detbitCardNum);
+                    if (currentUser != null)
+                    {
+                        break;
+                    }
+                    else 
+                    { 
+                        Console.WriteLine("Sorry Sir/Madam/behri, Card should not recongnized. Please try again :)");
+                    }
+                }
+                catch
+                {
+
+                    Console.WriteLine("Sorry Sir/Madam/behri, Card should not recongnized. Please try again :)");
+
+                }
+            }
+            //pin code
+            Console.WriteLine("Enter your Magical 4 digit Number/Code:");
+            int userpin = 0;
+
+            while (true)
+            {
+                try
+                {
+                    userpin = int.Parse(Console.ReadLine());
+                    //check in users list 
+                    if (currentUser.getpin() == userpin)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry Sir/Madam/behri, Incorrect Pin. Please try again :)");
+                    }
+                }
+                catch
+                {
+
+                    Console.WriteLine("Sorry Sir/Madam/behri, Incorrect Pin. Please try again :)");
+
+                }
+            }
+
+            //geting user name
+            Console.WriteLine($"wellcome {currentUser.getfirstname} :)");
+            int option = 0;
+            do
+            {
+                printOptions();
+                try
+                {
+                    option = int.Parse(Console.ReadLine());
+                }
+                catch 
+                {
+                    if (option == 1)
+                    {
+                        deposit(currentUser);
+                    }
+                    else if (option == 2)
+                    {
+                        withdraw(currentUser);
+
+                    }
+                    else if (option ==3)
+                    {
+                        balance(currentUser);
+                    }
+                    else if (option == 4)
+                    {
+                        break;
+                    }
+                }
+
+            } while (option !=4);
+            Console.WriteLine("Thank you! Hava a nice day :)");
+        }
+    }
+}
---
+++ atm/Program.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CardHolder
+{
+    string cardnum;
+    int pin;
+    string firstname;
+    string lastname;
+    double balance;
+
+    public CardHolder(string cardnum, int pin, string firstname, string lastname, double balance)
+    {
+        this.cardnum = cardnum;
+        this.pin = pin;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.balance = balance;
+    }
+
+    public string GetNum()
+    {
+        return cardnum;
+    }
+
+    public int GetPin()
+    {
+        return pin;
+    }
+
+    public string GetFirstName()
+    {
+        return firstname;
+    }
+
+    public string GetLastName()
+    {
+        return lastname;
+    }
+
+    public double GetBalance()
+    {
+        return balance;
+    }
+
+    public void SetNum(string newcardnum)
+    {
+        cardnum = newcardnum;
+    }
+
+    public void SetPin(int newpin)
+    {
+        pin = newpin;
+    }
+
+    public void SetFirstName(string newfirstname)
+    {
+        firstname = newfirstname;
+    }
+
+    public void SetLastName(string newlastname)
+    {
+        lastname = newlastname;
+    }
+
+    public void SetBalance(double newbalance)
+    {
+        balance = newbalance;
+    }
+
+    public static void Main(string[] args)
+    {
+        void PrintOptions()
+        {
+            Console.WriteLine("Welcome to ATM");
+            Console.WriteLine("Please select one of these options:");
+            Console.WriteLine("1. Deposit");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. Show balance");
+            Console.WriteLine("4. Exit");
+        }
+
+        void Deposit(CardHolder currentUser)
+        {
+            Console.WriteLine("How much $$ would you like to deposit?");
+            double depositAmount = double.Parse(Console.ReadLine());
+            currentUser.SetBalance(currentUser.GetBalance() + depositAmount);
+            Console.WriteLine($"Thank you for depositing $$. Your new balance is: {currentUser.GetBalance()}");
+        }
+
+        void Withdraw(CardHolder currentUser)
+        {
+            Console.WriteLine("How much $$ would you like to withdraw?");
+            double withdrawalAmount = double.Parse(Console.ReadLine());
+
+            if (currentUser.GetBalance() < withdrawalAmount)
+            {
+                Console.WriteLine("Sorry, you can't withdraw. You don't have enough balance.");
+            }
+            else
+            {
+                currentUser.SetBalance(currentUser.GetBalance() - withdrawalAmount);
+                Console.WriteLine("You're good to go! Thank you :)");
+            }
+        }
+
+        void ShowBalance(CardHolder currentUser)
+        {
+            Console.WriteLine($"Your current balance is: {currentUser.GetBalance()}");
+        }
+
+        List<CardHolder> cardHolders = new List<CardHolder>
+        {
+            new CardHolder("4238679324067835", 1343, "Sameed", "Behari", 17803.52),
+            new CardHolder("5483679329757835", 1243, "Shabbir", "Taliban", 17833.52),
+            new CardHolder("6783679367357835", 7343, "Usman", "binladir", 17853.52),
+            new CardHolder("7983679390257835", 2343, "Asad", "dhobe", 178.52),
+            new CardHolder("8903679389377835", 4343, "Kabber", "Pashah", 17383.52),
+            new CardHolder("9026679357857835", 5343, "Ayan", "bhai", 17853.52)
+        };
+
+        Console.WriteLine("Welcome Sir/Madam/Behari");
+        Console.WriteLine("I hope you use this ATM in good health :)");
+        Console.WriteLine("Please enter your debit card: ");
+        string debitCardNum = "";
+        CardHolder currentUser;
+
+        while (true)
+        {
+            try
+            {
+                debitCardNum = Console.ReadLine();
+                currentUser = cardHolders.FirstOrDefault(a => a.GetNum() == debitCardNum);
+
+                if (currentUser != null)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry Sir/Madam/Behari, Card not recognized. Please try again :)");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Sorry Sir/Madam/Behari, Card not recognized. Please try again :)");
+            }
+        }
+
+        Console.WriteLine("Enter your Magical 4 digit Number/Code:");
+        int userPin = 0;
+
+        while (true)
+        {
+            try
+            {
+                userPin = int.Parse(Console.ReadLine());
+
+                if (currentUser.GetPin() == userPin)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry Sir/Madam/Behari, Incorrect Pin. Please try again :)");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Sorry Sir/Madam/Behari, Incorrect Pin. Please try again :)");
+            }
+        }
+
+        Console.WriteLine($"Welcome {currentUser.GetFirstName()} :)");
+        int option = 0;
+
+        do
+        {
+            PrintOptions();
+
+            try
+            {
+                option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Deposit(currentUser);
+                        break;
+                    case 2:
+                        Withdraw(currentUser);
+                        break;
+                    case 3:
+                        ShowBalance(currentUser);
+                        break;
+                    case 4:
+                        Console.WriteLine("Thank you! Have a nice day :)");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+
+        } while (option != 4);
+    }
+}
